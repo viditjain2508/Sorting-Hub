2bc339c64904f3ad16c7e4a6bccda68fbb96ba64
viditjain2508 <rsvr516@gmail.com>
2024-01-08 19:10:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SortingHub from './SortingHub'
+const App = () => {
+    return (
+        <div>
+            <SortingHub/>
+        </div>
+    )
+}
+
+export default App
+
